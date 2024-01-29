7b12d8c2c5426b893d65fdd320d470a3ea1a83a4
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-01-29 09:48:08 +0700

    add context-content

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
+import {userContext} from './context'
+import Header from './context-header';
 
 export default function App() {
 
   return <>
-  <RefsFunc /> ,<br></br>, <RefsArray />,<br></br>,<MessageBox />; 
+ <userContext.Provider value={'Tom Jerry'}>
+  <Header/>
+ </userContext.Provider>
   </>
 }
